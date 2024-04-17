c33ca1d2b7e0ed92e5b6b51b5d2ec5a7bdf837c5
Maxime PIAU <maxime.piau@etu.univ-nantes.fr>
2024-04-17 19:06:04 +0000

    Update .gitlab-ci.yml

---
+++ gitlab.rb
@@ -0,0 +1,4 @@
+pages_external_url "https://e234117a.univ-nantes.io/portfoliomaximepiau/"
+gitlab_pages['enable'] = true
+gitlab_pages['status_uri'] = "/@status"
+gitlab_pages['listen_proxy'] = "193.52.104.60"
